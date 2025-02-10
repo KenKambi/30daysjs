2f70d714fb6c3981ca9b1cf3f84183b7f7a7255e
Ken Kambi <kenkambi43@gmail.com>
2025-02-10 15:11:26 +0300

    .value on input field

--- day3/day3.js
+++ day3/day3.js
@@ -1,13 +1,15 @@
 //alert("Hello");
 
 //let userName = prompt("What is youe name?", "");
-
 //alert("Hello " + userName);
 
+//Don't put .value at input coz it'll take the initial value being an empty string
 let input = document.querySelector('.input');
 let greeting = document.querySelector('.greeting');
 
 
 greeting.addEventListener('click', () => {
-    alert("Hello " + input);
+   //get the input value to display on the alert box
+    const inputValue = input.value;
+    alert("Hello " + inputValue);
 })
