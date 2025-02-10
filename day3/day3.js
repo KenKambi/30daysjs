8c9e50e28e014ac72e17e317396240f9d188a051
Ken Kambi <kenkambi43@gmail.com>
2025-02-10 15:43:39 +0300

    use of isNaN

--- day3/day3.js
+++ day3/day3.js
@@ -4,12 +4,22 @@
 //alert("Hello " + userName);
 
 //Don't put .value at input coz it'll take the initial value being an empty string
-let input = document.querySelector('.input');
-let greeting = document.querySelector('.greeting');
+let firstName = document.querySelector('.first-name');
+let secondName = document.querySelector('.second-name');
+let age = document.querySelector('.age');
 
+let greeting = document.querySelector('.greeting');
 
 greeting.addEventListener('click', () => {
    //get the input value to display on the alert box
-    const inputValue = input.value;
-    alert("Hello " + inputValue);
+    let theFirstName = firstName.value;
+    let theSecondName = secondName.value;
+    let theAge = isNaN(age.value);
+
+    if(theAge){
+        alert("Please enter a valid number!");
+    } else{
+        alert("Hello " + theFirstName +" "+ theSecondName +" "+ age.value );
+    }
+    console.log("sent");
 })
